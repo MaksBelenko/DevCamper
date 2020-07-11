a85d4b3daf7861cc6b4682ce69c300456153a220
MaksBelenko <maksim.belenko@gmail.com>
2020-07-11 22:26:26 +0100

    Added create bootcamp functionality; Added Json Parsing middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,9 +1,11 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps'});
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 };
 
 // @desc    Get single bootcamp
@@ -11,17 +13,30 @@ exports.getBootcamps = (req, res, next) => {
 // @access  Public
 
 exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show all bootcamp ${req.params.id}` });
+  res.status(200).json({
+    success: true,
+    msg: `Show all bootcamp ${req.params.id}`,
+  });
 };
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: 'Error adding new Bootcamp',
+    });
+  }
 };
 
 // @desc    Get single bootcamp
@@ -29,9 +44,10 @@ exports.createBootcamp = (req, res, next) => {
 // @access  Private
 
 exports.updateBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ${req.params.id}`,
+  });
 };
 
 // @desc    Delete bootcamp
@@ -39,7 +55,8 @@ exports.updateBootcamp = (req, res, next) => {
 // @access  Private
 
 exports.deleteBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ${req.params.id}`,
+  });
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 // const logger = require('./middleware/logger');
+const colors = require('colors');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
@@ -16,21 +17,30 @@ const bootcamps = require('./routes/bootcamps');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-// Dev Logging middleware
+
+
+// ---------------- Middleware ----------------
+app.use(express.json()); // Parse JSON
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+    app.use(morgan('dev')); // Dev Logging middleware
 }
 // app.use(logger);
 app.use('/api/v1/bootcamps', bootcamps); // Mount routes
 
+
+
+
 const server = app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    );
 });
+
+
 
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`);
-  server.close(() => process.exit(1));
-})
+    console.log(`Error: ${err.message}`.red);
+    server.close(() => process.exit(1));
+});
