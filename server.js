7abbc8ea6ab5cea2fd05b322c2677ce8642832e3
MaksBelenko <maksim.belenko@gmail.com>
2020-07-11 20:21:28 +0100

    Connected to atlas: Added mongoose and morgan

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Routes files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+// const logger = require('./middleware/logger');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Mount routes
-app.use('/api/v1/bootcamps', bootcamps);
+// Middleware
+// Dev Logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+// app.use(logger);
+app.use('/api/v1/bootcamps', bootcamps); // Mount routes
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+})
