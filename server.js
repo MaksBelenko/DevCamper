c65a3bbb088257d2626c7f538c5c8ecef39686fe
MaksBelenko <maksim.belenko@gmail.com>
2020-07-11 17:21:49 +0100

    Added controller and router

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
+
