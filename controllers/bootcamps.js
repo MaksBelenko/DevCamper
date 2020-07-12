6d855636e1b34a099354e2e7c1f959bbd7c4239c
MaksBelenko <maksim.belenko@gmail.com>
2020-07-12 15:10:32 +0100

    Added asyncHandler; Added slug and geocoder

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,108 +1,95 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
 
-exports.getBootcamps = async (req, res, next) => {
-  try {
-    const bootcamps = await Bootcamp.find();
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({
-      success: true,
-      count: bootcamps.length,
-      data: bootcamps,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
 
-exports.getBootcamp = async (req, res, next) => {
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const id = req.params.id;
 
-  try {
-    const bootcamp = await Bootcamp.findById(id);
+  const bootcamp = await Bootcamp.findById(id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404));
-    }
-
-    res.status(200).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Create new bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
 
-exports.createBootcamp = async (req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Get single bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 
-exports.updateBootcamp = async (req, res, next) => {
-  try {
-    const id = req.params.id;
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
 
-    const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+  const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404));
-    }
-
-    res.status(200).json({
-      success: true,
-      data: bootcamp,
-    });
-  } catch (error) {
-    next(error);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp,
+  });
+});
 
 // @desc    Delete bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private
 
-exports.deleteBootcamp = async (req, res, next) => {
-  try {
-    const id = req.params.id;
-    const bootcamp = await Bootcamp.findByIdAndDelete(id);
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const id = req.params.id;
+  const bootcamp = await Bootcamp.findByIdAndDelete(id);
 
-    if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404));
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {},
-    });
-  } catch (error) {
-    next(error);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id ${id} in the database`, 404)
+    );
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
